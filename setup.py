f30f583fb1e056ae6a06773398a5e451a455fb4f
Kaleiko <chase@kaleiko.net>
2022-04-10 16:07:37 -0700

    edited teh classifications and setup.py file

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ setup(
     version='0.1.0',
     description='Provides template utility functions as well as generate'
                 'base teßßmplate files used within web development frameworks',
-    url='git@github.com:Kaleiko/kutilities.git',
+    url='git+ssh://git@github.com/kaleiko/kutilities.git',
     author='Chase Kaleikilo',
-    email='code@kaleiko.net',
+    author_email='code@kaleiko.net',
     license='MIT License',
     packages=['kutilities'],
     zip_safe=False,
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Web Development and Data',
-        'License :: MIT License',  
-        'Operating System :: POSIX :: Linux',        
+        'Intended Audience :: Developers',
+        'License :: Freeware',  
+        'Operating System :: POSIX :: Linux',    
         'Programming Language :: Python :: 3.10',
     ],
 )
