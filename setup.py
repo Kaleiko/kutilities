e4f593e028ccb22433f193e3f42f0ce5f89ac74d
Kaleiko <chase@kaleiko.net>
2022-04-10 15:14:53 -0700

    initial package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='kutilities',
+    version='0.1.0',
+    description='Provides template utility functions as well as generate base template files
+                 'used within web development frameworks',
+    url='git@github.com:Kaleiko/kutilities.git',
+    author='Chase Kaleikilo',
+    email='code@kaleiko.net',
+    license='MIT License',
+    packages=['kutilities'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Web Development and Data',
+        'License :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.10',
+    ],
+)
